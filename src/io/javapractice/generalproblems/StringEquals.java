21aa50f259733fbf797792e87704072b30393d51
Pardeep <pk.raswant@gmail.com>
2018-10-15 02:08:25 +0530

    Changes Added

--- src/io/javapractice/generalproblems/StringEquals.java
+++ src/io/javapractice/generalproblems/StringEquals.java
@@ -5,28 +5,28 @@ public class StringEquals {
 	public static void main(String[] args){
 		String s1="pardeep",s2="pardeep";
 		
-		if(s1==s2){
-			System.out.println(true);
-		}
-		System.out.println(s1.equals(s2));
+		System.out.println(s1==s2);//true
+		System.out.println(s1.equals(s2));//true
 		
 		StringBuilder stringBuilder=new StringBuilder("pardeep");
 		StringBuilder stringBuilder2=new StringBuilder("pardeep");
 		
-		System.out.println(stringBuilder==stringBuilder2);
-		System.out.println(stringBuilder.equals(stringBuilder2));
+		System.out.println(stringBuilder==stringBuilder2);//false
+		System.out.println(stringBuilder.equals(stringBuilder2));//false
+		System.out.println(stringBuilder.toString().equals(stringBuilder2.toString()));//true
 		
 		StringBuffer stringBuffer=new StringBuffer("pardeep");
 		StringBuffer stringBuffer2=new StringBuffer("pardeep");
 		
-		System.out.println(stringBuffer==stringBuffer2);
-		System.out.println(stringBuffer.equals(stringBuffer2));
+		System.out.println(stringBuffer==stringBuffer2);//false
+		System.out.println(stringBuffer.equals(stringBuffer2));//false
+		System.out.println(stringBuffer.toString().equals(stringBuffer2.toString()));//true
 		
 		String string1=new String("pardeep");
 		String string2=new String("pardeep");
 		
-		System.out.println(string1==string2);
-		System.out.println(string1.equals(string2));
+		System.out.println(string1==string2);//false
+		System.out.println(string1.equals(string2));//true
 	}
 
 }
